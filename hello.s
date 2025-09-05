cf9ccf9b279a733847100a0db0dd2c2170b94d29
Ayush Biswas <biswas.ayush03@gmail.com>
2025-09-05 14:15:09 +0530

    Initial commit 😁

---
+++ hello.s
@@ -0,0 +1,14 @@
+        .global     _main
+
+        .text
+hwStr:  .asciz  "Hello, World!\n"
+
+        .align  2
+_main:
+        adr    x0, hwStr
+        bl      _printf
+
+        mov     x0, #0
+        bl      _exit
+
+        .data
---
+++ submit.py
@@ -0,0 +1,2 @@
+# This is a scratch file for writing the solution
+# in python and submitting on aoj for marking it done
