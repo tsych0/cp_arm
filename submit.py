bee19a5da2ec0ac91c39f78478665ce0f6ac9579
Ayush Biswas <biswas.ayush03@gmail.com>
2025-09-06 14:35:47 +0530

    Done 4 aoj questions

--- submit.py
+++ submit.py
@@ -1,2 +1,13 @@
 # This is a scratch file for writing the solution
 # in python and submitting on aoj for marking it done
+
+import sys
+
+t = int(input())
+
+for line in sys.stdin:
+    a, b, c = sorted(map(int, line.split()))
+    if a**2 + b**2 == c**2:
+        print("YES")
+    else:
+        print("NO")
